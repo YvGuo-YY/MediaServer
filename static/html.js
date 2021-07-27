13c658d68045dcfc2b70530638b24392fbf02700
panrunqiu@outlook.com <panrunqiu@outlook.com>
2021-07-27 16:33:30 +0800

    优化移动端显示效果

--- static/html.js
+++ static/html.js
@@ -1,6 +1,6 @@
 const file_html_data =  "<div class=\"mdc-card\"\n" +
                         "     style=\"cursor: pointer;background-color: #333333\">\n" +
-                        "    <img class=\"mdc-card__media mdc-card__media--16-9\" src=\"/getAssets?res=mime-type-icon/{1}&path={2}\" alt=\"cover\"/>\n" +
+                        "    <img class=\"mdc-card__media mdc-card__media--16-9\" src=\"/getAssets?res=mime-type-icon/{1}&path={2}\" style=\"width: 100%;\" alt=\"cover\"/>\n" +
                         "    <ul class=\"mdc-list\">\n" +
                         "        <li class=\"{6} mdc-list-item file-dialog-name__text demo-card__title\" style=\"padding-left: 20px;width: auto;align-items: baseline;\">\n" +
                         "            {0}\n" +
@@ -31,7 +31,7 @@ const file_html_data =  "<div class=\"mdc-card\"\n" +
                         "    </div>\n" +
                         "</div>";
 const directory_html_data = "<div class=\"mdc-card\" style=\"height: fit-content;cursor: pointer;background-color: #333333\" onclick=\"onItemClick('{0}','Directory','')\">\n" +
-                            "    <img class=\"mdc-card__media\" src=\"/getCover?cover={0}\" style=\"\" alt=\"{0}\"/>\n" +
+                            "    <img class=\"mdc-card__media\" src=\"/getCover?cover={0}\" style=\"width: 100%;\" alt=\"{0}\"/>\n" +
                             "    <span class=\"ipc-rating-star ipc-rating-star--baseAlt ipc-rating-star--imdb\" style=\"padding: 10px 5px;\"></span>\n" +
                             "    <div class=\"mdc-card-wrapper__text-section\">\n" +
                             "        <div class=\"demo-card__title\">{0}</div>\n" +
